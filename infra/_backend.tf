ff0bf35427f57ac98d1ad46e28b0c8d8df70bbae
Nga <92612527+NgaNaNa@users.noreply.github.com>
2025-05-13 13:32:37 +0930

    reorganises and simplies tf config files

--- infra/_backend.tf
+++ infra/_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "node-app-infra-tfstate-dev"
+    bucket         = "node-app-infra-tfstate-${var.envirnment}" #pulls in value from the var file
     key            = "terraform.tfstate"
     region         = "ap-southeast-2"
     dynamodb_table = "terraform-locks"
---
+++ infra/iam.tf
@@ -0,0 +1,11 @@
+data "aws_aim_policy_document" "ecs_instance_assume" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
