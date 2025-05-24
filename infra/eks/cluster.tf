6bb78b80207854c937e4131ab7a59c0eca26e9a1
Nga <92612527+NgaNaNa@users.noreply.github.com>
2025-05-24 21:42:09 +0930

    update module eks cluster version

--- infra/eks/cluster.tf
+++ infra/eks/cluster.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 20.0"
 
   cluster_name    = "${var.environment}-eks"
-  cluster_version = "1.29"
+  cluster_version = "1.30"
 
   vpc_id     = var.vpc_id
   subnet_ids = var.app_public_subnet_ids
